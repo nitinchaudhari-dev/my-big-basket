d98052e8758ab6ed23283c75aa5133b47eda58eb
nitin chaudhari <nitinchaudhari12349@gmail.com>
2025-05-17 12:26:37 +0700

    Update Vite config with base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // Add this line to ensure the app is deployed to the root of the domain
 })
