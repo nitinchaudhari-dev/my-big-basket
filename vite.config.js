15015ba0cae60241bd5b1967bb5ea5d3fb89c3a3
nitin chaudhari <nitinchaudhari12349@gmail.com>
2025-05-19 12:32:20 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Add this line to ensure the app is deployed to the root of the domain
-})
+});
